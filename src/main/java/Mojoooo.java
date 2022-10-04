17cbd8e41ebe9438d3500005e83e0b805c979611
amir8602 <amirreza8602@gmail.com>
2022-10-04 13:41:45 +0330

    first commit

---
+++ src/main/java/Mojoooo.java
@@ -0,0 +1,40 @@
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mojo(name = "dependency-counter", defaultPhase = LifecyclePhase.COMPILE)
+public class Mojoooo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Parameter(property = "scope")
+    String scope;
+
+
+//    public void execute() throws MojoExecutionException, MojoFailureException {
+//        List<Dependency> dependencies = project.getDependencies();
+//        long numDependencies = dependencies.stream()
+//                .filter(d -> (scope == null || scope.isEmpty()) || scope.equals(d.getScope()))
+//                .count();
+//        getLog().info("Number of dependencies: " + numDependencies);
+//    }
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        List<Dependency> dependencies = project.getDependencies();
+        for (Dependency dependency : dependencies) {
+            getLog().info("dep_name" +"  "+ dependency.getArtifactId()+ "  "+  dependency.getGroupId()+ "\n");
+        }
+        long numDependencies = dependencies.stream().count();
+        getLog().info("Number of dependencies: " + numDependencies);
+    }
+
+}
